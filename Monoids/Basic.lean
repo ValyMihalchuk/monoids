f25e99a901a96e9b0861699e660b3cf03650532b
Valentin Mikhalchuk <valy378@gmail.com>
2026-06-28 20:00:35 +0800

    basic vec

--- Monoids/Basic.lean
+++ Monoids/Basic.lean
@@ -1,4 +1,18 @@
 
-inductive Vec (α : Type) : Nat → Type u where
+inductive Vec (α : Type) : Nat → Type where
   | nil : Vec α 0
   | cons (a : α) {n : Nat} (v : Vec α n) : Vec α (n + 1)
+
+
+#check (Vec.cons 5 (Vec.cons 4 (Vec.nil)) : Vec Nat 2)
+
+
+def vappend : Vec a m → Vec a n → Vec a (n+m)
+  | Vec.nil, ys => ys
+  |(Vec.cons x xs), ys => Vec.cons x (vappend xs ys)
+
+#check Nat.add
+
+
+/-[1,2] ++ [3] = [1,2,3] -/
+#eval vappend (Vec.cons 1 (Vec.cons 2 (Vec.nil))) (Vec.cons 3 (Vec.nil))
